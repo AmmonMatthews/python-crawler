cce058cb7aefaa38a61bbf03c46c8dd696483d84
AmmonMatthews <ammonmatthews@gmail.com>
2020-12-16 22:34:40 -0700

    monster website crawled based of hardcoded url

--- monster.py
+++ monster.py
@@ -8,5 +8,17 @@ page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
 
 results = soup.find(id='ResultsContainer')
+job_elems = results.find_all('section', class_='card-content')
 
-print(results.prettify())
+for job in job_elems:
+  title_elem = job.find('h2', class_='title')
+  company_elem = job.find('div', class_='company')
+  location_elem = job.find('div', class_='location')
+  if None in (title_elem, company_elem, location_elem):
+    continue
+  print(title_elem.text.strip())
+  print(company_elem.text.strip())
+  print(location_elem.text.strip())
+  print()
+
+# print(results.prettify())
